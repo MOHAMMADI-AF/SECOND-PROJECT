6c0bbad438b0970d0a1ff175cfd04271a05073ed
Mohammad Akbar MOHAMMADI <MOHAMMADI@MOHAMMADI.ORG>
2022-12-21 10:06:48 -0500

    more updates and cleaning commented line

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -56,9 +56,11 @@ const reducer = combineReducers({
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
+
 const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
   ? JSON.parse(localStorage.getItem("shippingAddress"))
   : {};
